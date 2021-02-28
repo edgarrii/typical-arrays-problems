0048b949b7e071dedfe2550b549fc1c0c44b3527
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-02-28 20:59:34 +0300

    fix: again fix this 3 fails :)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 
 exports.min = function min (arr) {
+   if (Array.isArray(arr) == true) {
   if (arr.length == 0 || arr == null) return 0;
   let tempMin = arr[0];
   for (let i = 0; i < arr.length; i++) {
     if (tempMin > arr[i]) tempMin = arr[i];
   }
   return tempMin;
+} else return 0;
 }
 
 exports.max = function max (arr) {
-   if (arr.length == 0 || arr == null) return 0;
-  let tempMax = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > tempMax) tempMax = arr[i];
-  }
-  return tempMax;
+   if (Array.isArray(arr) == true) {
+    if (arr.length == 0 || arr == null) return 0;
+      let tempMax = arr[0];
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > tempMax) tempMax = arr[i];
+      }
+      return tempMax;
+  } else return 0;
 }
 
 exports.avg = function avg (arr) {
-    if (arr.length == 0 || arr == null) return 0;
+    if (Array.isArray(arr) == true) {
+  if (arr.length == 0 ) return 0;
   let sum = 0;
   let avg = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -26,4 +31,5 @@ exports.avg = function avg (arr) {
     avg = sum / arr.length;
   }
   return avg;
+} else return 0;
 }
